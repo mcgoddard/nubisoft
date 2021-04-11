a866198afa01e9430aa5b030c10cec038becc65f
Daniel J. Rollins <daniel@djrollins.com>
2021-04-11 18:49:32 +0100

    tune fear mechanics to make fear swings less drastic

--- Assets/Scripts/FearController.cs
+++ Assets/Scripts/FearController.cs
@@ -6,52 +6,58 @@ public class FearController : MonoBehaviour
     public LayerMask peonsLayer;
     public LayerMask bunniesLayer;
     private const float NEIGHBOUR_SEARCH_RADIUS = 1.0f;
-    private const float BUNNY_SEARCH_RADIUS = 1.0f;
+    private const float BUNNY_SEARCH_RADIUS = 0.5f;
     private const float FEAR_MAX = 1.0f;
     private const float FEAR_MIN = 0.0f;
-    private const float FEAR_DECAY_RATE = 0.2f;
-    private const float FEAR_TRANSFER_RATE = 1f;
+    private const float BASE_FEAR_DECAY_RATE = 0.2f;
+    private const float BASE_FEAR_TRANSFER_RATE = 0.5f;
     private const float FEAR_SACRIFICE_MAX = 0.6f;
     private const float FEAR_SACRIFICE_MIN = 0.4f;
     private const float BUNNY_DROP_BUFFER = 0.2f;
-    private float fear;
+    public float fear;
     private bool isCarryingBunny = false;
 
+    private float fearTransferRate;
 
     void Start() {
         fear = Random.Range(0.0f, 0.1f);
+        fearTransferRate = (1f - Random.Range(0.0f, 0.33f)) * BASE_FEAR_TRANSFER_RATE;
     }
 
     void Update() {
-        Collider2D[] neighbours = Physics2D.OverlapCircleAll(transform.position, NEIGHBOUR_SEARCH_RADIUS, peonsLayer);
+        var me = this.GetComponent<Collider2D>();
+        Collider2D[] neighbours = Physics2D
+            .OverlapCircleAll(transform.position, NEIGHBOUR_SEARCH_RADIUS, peonsLayer)
+            .Where(neighbour => neighbour != me)
+            .ToArray();
+
         Collider2D[] bunnies = Physics2D.OverlapCircleAll(transform.position, BUNNY_SEARCH_RADIUS, bunniesLayer);
 
-        // Calculate the number of bunnies surrounding the Peon. this is normalised by distance so that bunnies
-        // further away have a smaller impact on the fear decay
-        var surroundingBunnies = bunnies.Sum(bunny => {
-            return 1f - (this.transform.position - bunny.transform.position).magnitude / BUNNY_SEARCH_RADIUS;
-        });
+        var surroundingBunnies = bunnies.Count();
 
-        if(isCarryingBunny) {
-            // If we are carrying a bunny, we are _obviously_ petting it so should our fear should decay a little faster?
-            surroundingBunnies += 0.01f;
+        // if(isCarryingBunny) {
+        //     // If we are carrying a bunny, we are _obviously_ petting it so should our fear should decay a little faster?
+        //     surroundingBunnies += 0.01f;
+        // }
+
+        // Once we are terrified only bunnies can calm us down
+        if (fear > 0.9 && surroundingBunnies == 0) {
+            return; 
         }
 
-        // Calculate the normalised sum of the fear levels of surrounding Peons. The fear from Peons that are further away have
-        // a smaller impact on the fear transfer rate
-        var surroundingFear = neighbours.Average(it => {
-            var neighbour = it.GetComponent<Peon>();
-            var normalisedDistance = (this.transform.position - neighbour.transform.position).magnitude / NEIGHBOUR_SEARCH_RADIUS;
-            return 1f - (neighbour.GetComponent<FearController>().GetFearLevel() * normalisedDistance);
-        });
+        var medianFear = neighbours
+            .Select(it => {
+                var neighbour = it.GetComponent<Peon>();
+                return neighbour.GetComponent<FearController>().GetFearLevel();
+            })
+            .DefaultIfEmpty(0f)
+            .Max();
 
-        // If everyone around us is chill, we shouldn't try to match there fear, Only bunnies and time can reduce our fear-level;
-        if (surroundingFear > fear) {
-            fear = Mathf.Lerp(fear, surroundingFear, FEAR_TRANSFER_RATE * Time.deltaTime);
+        if (medianFear > fear) {
+            fear = Mathf.Lerp(fear, medianFear, fearTransferRate * Time.deltaTime);
             fear = Mathf.Clamp(fear, FEAR_MIN, FEAR_MAX);
         } else {
-            // Decay fear based on how many bunnies are surrounding us
-            fear = Mathf.Lerp(fear, FEAR_MIN, (surroundingBunnies + FEAR_DECAY_RATE) * Time.deltaTime);
+            fear = Mathf.Lerp(fear, FEAR_MIN, (surroundingBunnies / 10) + BASE_FEAR_DECAY_RATE * Time.deltaTime);
         }
     }
 
@@ -66,5 +72,9 @@ public class FearController : MonoBehaviour
 
     public float GetFearLevel() {
         return fear;
+    }
+
+    public void OnMouseDown() {
+        fear = FEAR_MAX;
     }
 }
