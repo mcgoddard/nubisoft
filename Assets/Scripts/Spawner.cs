9ee634eeb3ab641cdd8a43b66fecae1fb971ce65
Daniel J. Rollins <daniel@djrollins.com>
2021-04-11 18:50:14 +0100

    spawn bunnies witha right click

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -10,12 +10,14 @@ public class Spawner : MonoBehaviour
     public GameObject bunnyPrefab;
     public int peonCount = 50;
     private const float MAP_SIZE = 20.0f;
-    private const float BUNNY_SPAWN_TIMEOUT = 2f;
+    private Bounds mapBounds;
+    private const float BUNNY_SPAWN_RATE = 0.25f;
     private float timeSinceLastBunnySpawn = 0f;
     private GameObject[] peons;
 
     void Start()
     {
+        mapBounds = new Bounds(Vector3.zero, new Vector3(MAP_SIZE, MAP_SIZE, 0));
         Random.InitState((int)System.DateTime.Now.Ticks);
         peons = Enumerable
             .Range(0, peonCount)
@@ -24,24 +26,35 @@ public class Spawner : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        if (timeSinceLastBunnySpawn >= BUNNY_SPAWN_TIMEOUT) {
-            timeSinceLastBunnySpawn = 0f;
-            SpawnAtRandomPosition(bunnyPrefab);
-            UiUpdate.bunnies += 1;
-        } else {
-            timeSinceLastBunnySpawn += Time.deltaTime;
+        timeSinceLastBunnySpawn -= Time.deltaTime;
+
+        if (timeSinceLastBunnySpawn <0 && Input.GetMouseButton(1))
+        {
+            var mousePosition = UnitSelector.GetMouseWorldPosition();
+
+            if (mapBounds.Contains(mousePosition))
+            {
+                var offset = Random.insideUnitCircle / 10;
+                var spawnLocation = mousePosition + new Vector3(offset.x, offset.y, 0);
+                SpawnBunny(spawnLocation);
+                UiUpdate.bunnies += 1;
+            }
+
+            timeSinceLastBunnySpawn = BUNNY_SPAWN_RATE;
         }
 
         UiUpdate.fear = peons.Average(peon => peon.GetComponent<FearController>().GetFearLevel());
     }
 
-    public void SpawnBunny(Vector3 position) {
+    public void SpawnBunny(Vector3 position)
+    {
         Instantiate(bunnyPrefab, position, Quaternion.identity, this.transform);
     }
 
-    GameObject SpawnAtRandomPosition(GameObject prefab) {
+    GameObject SpawnAtRandomPosition(GameObject prefab)
+    {
         float randomX = (Random.value * MAP_SIZE) - (MAP_SIZE / 2.0f);
         float randomY = (Random.value * MAP_SIZE) - (MAP_SIZE / 2.0f);
         return Instantiate(prefab, new Vector3(randomX, randomY, 0), Quaternion.identity, this.transform);
