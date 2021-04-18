6d943541f18d7293443c009a0be30c8d95b26115
Mike Goddard <mikeygiom@gmail.com>
2021-04-18 14:45:20 +0100

    Added time to the UI, UI prefab on all scenes

--- Assets/Scripts/UiUpdate.cs
+++ Assets/Scripts/UiUpdate.cs
@@ -10,24 +10,29 @@ public class UiUpdate : MonoBehaviour
     public static int kills = 0;
     public static int bunnies = 0;
     public static float fear = 5.0f;
+    public static float time = 0.0f;
     private Text sacrificesText;
     private Text killsText;
     private Text fearText;
     private Text bunniesText;
+    private Text timeText;
     void Start()
     {
         sacrificesText = this.transform.Find("Sacrifices").GetComponent<Text>();
         killsText = this.transform.Find("Kills").GetComponent<Text>();
         fearText = this.transform.Find("Fear").GetComponent<Text>();
         bunniesText = this.transform.Find("Bunnies").GetComponent<Text>();
+        timeText = this.transform.Find("Time").GetComponent<Text>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        time += Time.deltaTime;
         sacrificesText.text = "Bunnies Sacrificed: " + sacrifices;
         killsText.text = "Bunnies Killed: " + kills;
         fearText.text = "Fear Level: " + fear;
         bunniesText.text = "Bunnies: " + bunnies;
+        timeText.text = "Time: " + (int)time;
     }
 }
