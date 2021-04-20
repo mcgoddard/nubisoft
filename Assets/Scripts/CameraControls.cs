d8f40e70abd7d65e4b8f7358e8cddd225975dd28
Daniel J. Rollins <daniel@djrollins.com>
2021-04-20 17:45:53 +0100

    constrain camera pan to map bounds

--- Assets/Scripts/CameraControls.cs
+++ Assets/Scripts/CameraControls.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 [RequireComponent(typeof(Camera))]
 public class CameraControls : MonoBehaviour
 {
+    private Bounds cameraBounds;
     private const int EDGE_PAN_BUFFER = 100;
     private const float CAMERA_PAN_SPEED = 10;
     private const float MIN_ZOOM_LEVEL = 3.5f;
@@ -14,37 +15,48 @@ public class CameraControls : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        this.camera = this.GetComponent<Camera>();
+        camera = this.GetComponent<Camera>();
+        cameraBounds =  new Bounds(Vector3.zero, new Vector3(20, 20f, Mathf.Infinity));
     }
 
     // Update is called once per frame
     void Update()
     {
+        // Zoom camera based on how much the scroll wheel has moved
         this.camera.orthographicSize = Mathf.Clamp(this.camera.orthographicSize - Input.mouseScrollDelta.y, MIN_ZOOM_LEVEL, MAX_ZOOM_LEVEL);
 
-        if (Input.GetMouseButtonDown(2)) {
-            middleMouseDownPosition = Input.mousePosition;
-        }
-
         if (Input.GetMouseButton(2)) {
+            if (Input.GetMouseButtonDown(2)) {
+                // If this is the first frame that middle mouse button was pressed,
+                // clobber the last position as it is no longer valid
+                middleMouseDownPosition = Input.mousePosition;
+            }
+
+            // Move camera based on the offset from the current mouse position and the mouse
+            // position in the previous frame.
             Vector3 target = -Camera.main.ScreenToViewportPoint(Input.mousePosition - middleMouseDownPosition) * CAMERA_PAN_SPEED;
-
             transform.Translate(target, Space.World);
+
+            // Update to the current mouse position so we so we can compute the new delta on the next frame
             middleMouseDownPosition = Input.mousePosition;
+        } else {
+            // Handle edge pan base on mouse position
+            if (Input.mousePosition.y >= Screen.height - EDGE_PAN_BUFFER) {
+                transform.position += Vector3.up * CAMERA_PAN_SPEED * Time.deltaTime;
+            }
+            if (Input.mousePosition.y <= EDGE_PAN_BUFFER) {
+                transform.position += Vector3.down * CAMERA_PAN_SPEED * Time.deltaTime;
+            }
+            if (Input.mousePosition.x >= Screen.width - EDGE_PAN_BUFFER) {
+                transform.position += Vector3.right * CAMERA_PAN_SPEED * Time.deltaTime;
+            }
+            if (Input.mousePosition.x <= EDGE_PAN_BUFFER) {
+                transform.position += Vector3.left * CAMERA_PAN_SPEED * Time.deltaTime;
+            }
         }
 
-        if (Input.mousePosition.y >= Screen.height - EDGE_PAN_BUFFER) {
-            this.transform.position += Vector3.up * CAMERA_PAN_SPEED * Time.deltaTime;
-        }
-        if (Input.mousePosition.y <= EDGE_PAN_BUFFER) {
-            this.transform.position += Vector3.down * CAMERA_PAN_SPEED * Time.deltaTime;
-        }
-        if (Input.mousePosition.x >= Screen.width - EDGE_PAN_BUFFER) {
-            this.transform.position += Vector3.right * CAMERA_PAN_SPEED * Time.deltaTime;
-        }
-        if (Input.mousePosition.x <= EDGE_PAN_BUFFER) {
-            this.transform.position += Vector3.left * CAMERA_PAN_SPEED * Time.deltaTime;
-        }
+        // Constrain the new position to fit within the camera bounds
+        transform.position = cameraBounds.ClosestPoint(transform.position);
     }
     public static Vector3 GetMouseWorldPosition()
     {
